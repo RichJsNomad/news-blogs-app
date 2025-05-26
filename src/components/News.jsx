275b8ab0cffa6da5fc0b1132a28f83d086be192d
Aleks <karaeeeani@gmail.com>
2025-05-26 13:18:36 +0300

    created the project layout

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import Weather from "./Weather";
+import Calendar from "./Calendar";
 
 const News = () => {
-  return <div>News</div>;
+  return (
+    <div className="news">
+      <header className="news-header">News Header</header>
+      <div className="news-content">
+        <div className="navbar">
+          <div className="user">User</div>
+          <nav className="categories">Categories</nav>
+        </div>
+        <div className="news-section">
+          <div className="headline">Headline</div>
+          <div className="news-grid">News Grid</div>
+        </div>
+        <div className="my-blogs">My blogs</div>
+        <Weather />
+        <Calendar />
+        <footer className="news-footer">Footer</footer>
+      </div>
+    </div>
+  );
 };
 
 export default News;
