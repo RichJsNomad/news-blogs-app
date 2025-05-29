281232d19757791b02bf85598b8fa4aa195e11c6
Aleks <karaeeeani@gmail.com>
2025-05-29 15:53:51 +0300

    customized user profile and categories styles

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Weather from "./Weather";
 import Calendar from "./Calendar";
 import "../styles/news.css";
+import userPhoto from "../assets/images/profile-photo.jpg";
 
 const News = () => {
   return (
@@ -19,8 +20,45 @@ const News = () => {
       </header>
       <div className="news-content">
         <div className="navbar">
-          <div className="user">User</div>
-          <nav className="categories">Categories</nav>
+          <div className="user">
+            <img src={userPhoto} alt="User Img" />
+            <p>RichJsNomad's Blog</p>
+          </div>
+          <nav className="categories">
+            <h1 className="nav-heading">Categories</h1>
+            <div className="nav-links">
+              <a href="" className="nav-link">
+                General
+              </a>
+              <a href="" className="nav-link">
+                World
+              </a>
+              <a href="" className="nav-link">
+                Business
+              </a>
+              <a href="" className="nav-link">
+                Technology
+              </a>
+              <a href="" className="nav-link">
+                Entartainment
+              </a>
+              <a href="" className="nav-link">
+                Sports
+              </a>
+              <a href="" className="nav-link">
+                Science
+              </a>
+              <a href="" className="nav-link">
+                Health
+              </a>
+              <a href="" className="nav-link">
+                Nation
+              </a>
+              <a href="" className="nav-link">
+                Bookmarks <i className="fa-regular fa-bookmark"></i>
+              </a>
+            </div>
+          </nav>
         </div>
         <div className="news-section">
           <div className="headline">Headline</div>
