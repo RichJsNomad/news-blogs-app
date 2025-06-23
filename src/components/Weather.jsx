7003baa02407609b587871e185e8b2bf42dae7e7
Aleks <karaeeeani@gmail.com>
2025-06-23 11:02:16 +0300

    created the Weather component and all hardcoded jsx related to it

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -2,7 +2,25 @@ import React from "react";
 import "../styles/weather.css";
 
 const Weather = () => {
-  return <div className="weather">Погода</div>;
+  return (
+    <div className="weather">
+      <div className="search">
+        <div className="search-top">
+          <i className="fa-solid fa-location-dot"></i>
+          <div>Нью-Йорк, США</div>
+        </div>
+        <div className="search-location">
+          <input type="text" placeholder="Введи Локацию" />
+          <i className="fa-solid fa-magnifying-glass"></i>
+        </div>
+      </div>
+      <div className="weather-data">
+        <i className="bx bxs-sun"></i>
+        <div className="weather-type">Солнечно</div>
+        <div className="temp">24°C</div>
+      </div>
+    </div>
+  );
 };
 
 export default Weather;
